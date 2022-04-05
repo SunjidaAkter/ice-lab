5adaefac56c87e35f5f8880e4d3ba168dd234a85
SunjidaAkter <sanjidabintesayid7@gmail.com>
2022-04-05 20:25:17 +0600

    about edited

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className='d-flex justify-content-center'>
-            <img src="https://travesia.axiomthemes.com/wp-content/uploads/2017/09/img_hotspot.png" alt="" />
+        <div className='d-flex justify-content-center mt-5'>
+            <img src="https://nmgprod.s3.amazonaws.com/media/files/ac/8f/ac8fedfcc7a137703c4e81de6c84e65d/cover_image_1615470651.jpg.760x400_q85_crop_upscale.jpg" alt="" />
         </div>
     );
 };
